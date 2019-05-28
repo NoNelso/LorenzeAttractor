1c3d56b81b44a0a454044ee27386a3380afa1b16
NoNelso <jasonr4nd@gmail.com>
2019-05-28 13:52:03 -0400

    pulled formulas from wikipedia for lorenze attraction

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   createCanvas(600, 500);
+  float x y, z, a, b, c;
+  float dx, dy, dz;
+  x = y = z = 0;
+  a = b = c = 1;
 }
 
 function draw() {
+  dx = a * (y - x);
+  x += dx;
+  dy = x * (b - z) - y;
+  y += dy;
+  dz = x * y - c * z;
+  z += dz;
+}
 
+function line() {
+  this.show
 }
