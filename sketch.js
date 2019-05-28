12bcd355c03698b3885988ecf7b654c7569ff1dd
NoNelso <jasonr4nd@gmail.com>
2019-05-28 17:46:55 -0400

    added colors and camera

--- sketch.js
+++ sketch.js
@@ -10,23 +10,44 @@ let dt = 0.01;
 let dx, dy, dz;
 
 let s = 5;
+let h = 0;
 
 let dot = new Array()
 
 function setup() {
-  createCanvas(600, 500);
-  background(0);
+  createCanvas(600, 500, WEBGL);
+  colorMode(HSB);
 }
 
 function draw() {
+  background(0);
+
   dx = a * (y - x);
   x = x + (dx * dt);
+
   dy = x * (b - z) - y;
   y = y + (dy * dt);
+
   dz = x * y - c * z;
   z = z + (dz * dt);
-  translate(width / 2, height / 2);
-  stroke(255);
-  point(s * x, s * y);
 
+  dot.push(new p5.Vector(x, y, z));
+
+  let camX = map(mouseX, 0, width, -200, 200);
+  let camY = map(mouseY, 0, height, -200, 200);
+  camera(camX, camY, (height / 2.0) / tan(PI * 30.0 / 180.0), 0, 0, 0, 0, 1, 0);
+
+  scale(s);
+  stroke(255);
+  noFill();
+
+  h = 0;
+  beginShape();
+  for (let v of dot) {
+    stroke(h, 255, 255);
+    vertex(v.x, v.y, v.z);
+    if (h > 254) h = 0;
+    else h += .1;
+  }
+  endShape();
 }
