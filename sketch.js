921aa1debbe46c8c253722fbc60c6e8e3209daf2
NoNelso <jasonr4nd@gmail.com>
2019-05-28 15:57:28 -0400

    shifted variables out of setup to stop error messages

--- sketch.js
+++ sketch.js
@@ -1,21 +1,22 @@
+let x = 0.01;
+let y = 0.0;
+let z = 0.0;
+let a = 10.0;
+let b = 28;
+let c = 8.0 / 3.0;
+let dt = 0.01;
+let dx, dy, dz;
+
 function setup() {
   createCanvas(600, 500);
   background(0);
-  let x = 0.0;
-  let y = 0.0;
-  let z = 0.0;
-  let a = 10.0;
-  let b = 28;
-  let c = 8.0 / 3.0;
-  let dx, dy, dz, dt;
 }
 
 function draw() {
-  dt = 0.01;
   dx = a * (y - x);
   x = x + (dx * dt);
   dy = x * (b - z) - y;
   y = y + (dy * dt);
   dz = x * y - c * z;
-  z = y + (dz * dt);
+  z = z + (dz * dt);
 }
