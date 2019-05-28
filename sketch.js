f5bd40e8431557857620a77c3aadd9fe583cd4f2
NoNelso <jasonr4nd@gmail.com>
2019-05-28 13:56:23 -0400

    added dt to scale calculation & black background.

--- sketch.js
+++ sketch.js
@@ -1,20 +1,20 @@
 function setup() {
   createCanvas(600, 500);
+  background(0);
   float x y, z, a, b, c;
-  float dx, dy, dz;
+  float dx, dy, dz, dt;
   x = y = z = 0;
-  a = b = c = 1;
+  a = 10;
+  b = 28;
+  c = 8.0 / 3.0;
+  dt = 1;
 }
 
 function draw() {
   dx = a * (y - x);
-  x += dx;
+  x += (dx * dt);
   dy = x * (b - z) - y;
-  y += dy;
+  y += (dy * dt);
   dz = x * y - c * z;
-  z += dz;
-}
-
-function line() {
-  this.show
+  z += (dz * dt);
 }
